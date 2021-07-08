75501e8976f645ac807dd37f02b7f22ee71237b6
akashdk21 <60746201+akashdk21@users.noreply.github.com>
2021-07-08 14:29:12 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+#add fn
+def add(x,y):
+    return x+y
+#subtract fn
+def subtract(x,y):
+    z=x-y
+    return z
+#multiply fn
+def multiply(x,y):
+    return x*y
+#divide fn
+def divide(x,y):
+    if y==0:
+        return DIVIDE_BY_ZERO
+    else:
+        return x/y
+#square fn
+def square(x):
+    pass
